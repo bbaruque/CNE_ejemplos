2208f49c58fafa548ec12a28287eb91a48641da6
Bruno Baruque <bbaruque@ubu.es>
2019-11-22 18:35:13 +0100

    Pequeños errores de codigo corregidos.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     toolbox = base.Toolbox()
     
     # Se debe permitir cambiar el fichero de entrada de datos facilmente
-    experiments.LecturaDatos.carga_datos('./experiments/datos2.csv')    
+    experiments.LecturaDatos.carga_datos('./experiments/datos/datos2.csv')    
     evol_simple.CicloEvolutivo.realizaEvolucion(toolbox,[])
     
     # Se deberán incluir los metodos necesarios para almacenar la solución
